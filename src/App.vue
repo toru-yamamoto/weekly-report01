eae4d2fd1c873c7b8529ec0a4b84ef86dad4d9a1
Toru Yamamoto <toruyamamoto@Toru-no-MacBook-Pro.local>
2017-07-10 11:33:04 +0900

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
 <template>
   <div id="app">
+      <ul>
+          <item v-for="item in items" :item="item"></item>
+      </ul>
+      <div>
+          <input type="text" v-model="inputTitle">
+          <button @click="addItem">追加</button>
+      </div>
+  </div>
+</template>
+<script>
+import Item from './Item.vue';
+export default {
+    name: 'app',
+    components: { Item },
+    data () {
+        return {
+            inputTitle: "",
+            items: [
+                {is_do: false, title: 'タスク1'},
+                {is_do: true, title: 'タスク2'},
+                {is_do: false, title: 'タスク3'}
+            ]
+        }
+    },
+    methods: {
+        addItem() {
+            this.items.push({
+                title: this.inputTitle,
+                is_do: false
+            });
+        }
+    }
+}
+</script>
+
+<!--
+<template>
+  <div id="app">
     <img src="./assets/logo.png">
     <h1></h1>
     <h2>Essential Links</h2>
@@ -58,3 +96,4 @@ a {
   color: #42b983;
 }
 </style>
+-->
---
+++ src/Item.vue
@@ -0,0 +1,21 @@
+<template>
+    <li v-bind:class="{ 'is-do': item.is_do }" @click="done(item)">{{ item.title }}</li>
+</template>
+<script>
+export default {
+    props: ['item'],
+    methods: {
+        done(item) {
+            item.is_do = !item.is_do;
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+li {
+    cursor: pointer;
+}
+li.is-do {
+    text-decoration: line-through;
+}
+</style>
